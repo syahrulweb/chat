6169c9d64847e0db7f2a0a77cd78182be62108b9
syahrulweb <sahrulloji05@gmail.com>
2025-09-25 03:32:18 +0700

    Initial commit: frontend WebsiteKomputer

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/chat/', // 👈 penting biar asset/js/css ketemu
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://chatbot-backend-nine-omega.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 })
